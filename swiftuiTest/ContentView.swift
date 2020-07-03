8bfe803dd0b4cae36bd9af498c0e9667fe91713e
Shota Takinami <shota_t@i.softbank.jp>
2020-07-03 13:15:01 +0900

    Initial Commit

---
+++ swiftuiTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiTest
+//
+//  Created by 滝浪翔太 on 2020/07/03.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
