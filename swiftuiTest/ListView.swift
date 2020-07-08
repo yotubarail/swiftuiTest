591f01c41782c6d19d6322bb8e59e123daa3db29
Shota Takinami <shota_t@i.softbank.jp>
2020-07-08 10:56:39 +0900

    ListViewでHStackを使った表示とSectionの表示

--- swiftuiTest/ListView.swift
+++ swiftuiTest/ListView.swift
@@ -9,13 +9,30 @@
 import SwiftUI
 
 struct ListView: View {
+    let number = ["1", "2", "3", "4"]
+    
     var body: some View {
         List {
             Text("リスト")
             Text("2行目")
             Image("logo")
+            HStack() {
+                Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .clipped()
+            Text("画像とテキストを表示")
+            }
+            Section(header: Text("セクション")){
+                Text("簡単")
+            }
+            Section(header: Text("数字を表示")) {
+                ForEach(0..<number.count) { index in
+                    Text(self.number[index])
+                }
+            }
         }
-        
     }
 }
 
