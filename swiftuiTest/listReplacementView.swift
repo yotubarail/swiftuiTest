34dfe5c37e52f08d97bccda5744ef8c13de2dd2c
Shota Takinami <shota_t@i.softbank.jp>
2020-07-10 15:47:11 +0900

    メモの追加

--- swiftuiTest/listReplacementView.swift
+++ swiftuiTest/listReplacementView.swift
@@ -14,7 +14,6 @@ struct listReplacementView: View {
     @State private var meats = ["鶏", "豚", "牛"]
     @State private var birds = ["鶏", "鳩", "雉"]
     @State private var phones = ["iPhone", "ZenFone", "Google Pixel"]
-//    var images: [Image] = [Image("logo"), Image("logo")]
     
     var body: some View {
         NavigationView {
@@ -24,7 +23,8 @@ struct listReplacementView: View {
                     ForEach(meats, id: \.self) { meat in
                         HStack {
                             Text(meat)
-                        } .id(UUID())
+                        }
+                        .id(UUID())
                     }
                     .onMove(perform: rowReplace)
                 }
@@ -33,7 +33,8 @@ struct listReplacementView: View {
                     ForEach(birds, id: \.self) { bird in
                         HStack {
                             Text(bird)
-                        } .id(UUID())
+                        }
+                        .id(UUID())
                     }
                     .onDelete(perform: rowDelete)
                 }
@@ -41,27 +42,35 @@ struct listReplacementView: View {
                 Section(header: Text("入れ替えも削除も可能なリスト")) {
                     ForEach(phones, id: \.self) { phone in
                         HStack {
+                            Image("logo")
+                                .resizable()
+                                .frame(width: 20, height: 20)
                             Text(phone)
-                        } .id(UUID())
+                        }
+                        .id(UUID())
                     }
                     .onMove(perform: phoneReplace)
                     .onDelete(perform: phoneDelete)
                 }
+                
+                
             }
             .navigationBarTitle("動作可能なリスト", displayMode: .inline)
             .navigationBarItems(trailing: EditButton())
         }
     }
     
-    
+    // 入れ替え可能なリスト
     func rowReplace(_ from: IndexSet, _ to: Int) {
         meats.move(fromOffsets: from, toOffset: to)
     }
     
+    // 削除可能なリスト
     func rowDelete(offsets: IndexSet) {
         birds.remove(atOffsets: offsets)
     }
     
+    // 入れ替えも削除も可能なリスト
     func phoneReplace(_ from: IndexSet, _ to: Int) {
         phones.move(fromOffsets: from, toOffset: to)
     }
