45cde7cb2c74c737f64eed7bc42340686a2f68fe
Shota Takinami <shota_t@i.softbank.jp>
2020-07-27 16:42:40 +0900

    connpass APIを使ったListの追加

---
+++ swiftuiTest/connpassList/connpassRowView.swift
@@ -0,0 +1,41 @@
+//
+//  connpassRowView.swift
+//  swiftuiTest
+//
+//  Created by 滝浪翔太 on 2020/07/27.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+struct connpassRowView: View {
+    
+    var eventData: Event
+    let formatter = ISO8601DateFormatter()
+
+    var body: some View {
+        
+        HStack {
+            Text(eventData.date)
+            VStack(alignment: .leading) {
+                Text(eventData.title)
+                    .font(.system(size: 25))
+                    .padding(.bottom,20)
+                Text(eventData.place)
+                    .font(.system(size: 15))
+            }
+            Spacer()
+//            VStack(alignment: .leading) {
+//                Text("参加:" + String(eventData.people))
+//                Text("定員:" + String(eventData.limit))
+//            }
+        }
+    }
+}
+
+
+struct connpassRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        connpassRowView(eventData: mockEventsData[0])
+    }
+}
