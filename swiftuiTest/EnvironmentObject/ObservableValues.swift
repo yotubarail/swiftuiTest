69abf6e952420de5cf667ae09b75a588b5bf3fea
Shota Takinami <shota_t@i.softbank.jp>
2020-10-30 13:22:05 +0900

    New Group

---
+++ swiftuiTest/EnvironmentObject/ObservableValues.swift
@@ -0,0 +1,14 @@
+//
+//  ObservableValues.swift
+//  swiftuiTest
+//
+//  Created by 滝浪翔太 on 2020/10/30.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+class ObservableValues: ObservableObject {
+    
+    @Published var numerics = 0.0
+}
---
+++ swiftuiTest/EnvironmentObject/SliderIView.swift
@@ -0,0 +1,21 @@
+//
+//  SliderIView.swift
+//  swiftuiTest
+//
+//  Created by 滝浪翔太 on 2020/10/30.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+struct SliderIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderIView()
+    }
+}
